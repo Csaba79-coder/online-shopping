5e11051cdc25e2328b41f555cd1142bd1ece1182
Csaba79-coder <csabavadasz79@gmail.com>
2023-02-07 00:09:33 +0100

    created Discovery Server (Eureka - Netflix)

--- inventory-service/src/main/java/com/csaba79coder/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/csaba79coder/inventoryservice/InventoryServiceApplication.java
@@ -2,10 +2,11 @@ package com.csaba79coder.inventoryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
-
 
     public static void main(String[] args) {
         SpringApplication.run(InventoryServiceApplication.class, args);
