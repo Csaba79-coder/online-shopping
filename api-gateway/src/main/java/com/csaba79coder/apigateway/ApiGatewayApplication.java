b785b1b4e78f2103163ddb1eb00949fa6b22a948
Csaba79-coder <csabavadasz79@gmail.com>
2023-02-08 10:31:24 +0100

    setup discovery server for API Gateway

--- api-gateway/src/main/java/com/csaba79coder/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/csaba79coder/apigateway/ApiGatewayApplication.java
@@ -6,7 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ApiGatewayApplication {
+public class
+ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
